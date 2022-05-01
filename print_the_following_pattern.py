edfa07f37399a8ced3cba509f2eb290366868ebc
Sanjay kumar <97424311+Sanjay21A91A05E0@users.noreply.github.com>
2022-05-01 16:58:53 +0530

    a=int(input()) for i in range (1,a+1):     for j in range(1,a+1):         j=64+i         print("%c"%j,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,6 @@
 a=int(input())
-for i in range(1,a+1):
-    for j in range(a,0,-1):
-        print(j,end=' ')
+for i in range (1,a+1):
+    for j in range(1,a+1):
+        j=64+i
+        print("%c"%j,end=' ')
     print()
