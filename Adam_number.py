c2979c5b99ebb298596f04552cb79456003a2e5a
Sanjay kumar <97424311+Sanjay21A91A05E0@users.noreply.github.com>
2022-05-02 19:34:04 +0530

    a=int(input()) s=0 reva=0 sqa=a*a while(a):     d=a%10     s=s*10+d     a//=10 sqs=s*s revsqs=0 while(sqs):     d=sqs%10     revsqs=revsqs*10+d     sqs//=10 if(sqa==revsqs):     print('True') else:     print('False')

---
+++ Adam_number.py
@@ -0,0 +1,19 @@
+a=int(input())
+s=0
+reva=0
+sqa=a*a
+while(a):
+    d=a%10
+    s=s*10+d
+    a//=10
+sqs=s*s
+revsqs=0
+while(sqs):
+    d=sqs%10
+    revsqs=revsqs*10+d
+    sqs//=10
+if(sqa==revsqs):
+    print('True')
+else:
+    print('False')
+    
