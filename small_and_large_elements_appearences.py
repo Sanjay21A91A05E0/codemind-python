7ab6e7ce8326d7da496c86308693ea3970406dfc
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-07-11 20:04:30 +0530

    a=list(input().replace(' ','')) print(min(a),a.count(min(a)),max(a),a.count(max(a)),end=' ')

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,5 +1,2 @@
-import collections
 a=list(input().replace(' ',''))
-d=collections.Counter(a)
-d=sorted(d)
-print(d[0],a.count(d[0]),d[-1],a.count(d[-1]))
+print(min(a),a.count(min(a)),max(a),a.count(max(a)),end=' ')
