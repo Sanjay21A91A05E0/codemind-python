59f7bbefded7f4b0b0363f75e52cca41465c294b
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-07-11 20:11:09 +0530

    a=input().split() s=a[len(a)-1] k=min(s) if k and k.lower() in s:     print(k.lower()) else:     print(k)

--- minimum_elemnt_from_a_string.py
+++ minimum_elemnt_from_a_string.py
@@ -1,9 +1,7 @@
-a=list(map(str,input().split()))
-a=a[::-1]
-for i in a:
-    c=min(i)
-    if c and c.lower() in i:
-        print(c.lower())
-        break
-    else:
-        print(c)
+a=input().split()
+s=a[len(a)-1]
+k=min(s)
+if k and k.lower() in s:
+    print(k.lower())
+else:
+    print(k)
