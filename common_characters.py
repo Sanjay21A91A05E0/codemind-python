67bba5dd0b50e40a58915c4f645096b3a00ea559
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-07-14 11:37:42 +0530

    a=list(input().lower().replace(' ','')) b=list(input().lower().replace(' ','')) d=[] for i in a:     if i in b and i not in d:         d.append(i) d.sort() d=''.join(d) print(d) if d else print('-1')

--- common_characters.py
+++ common_characters.py
@@ -1,13 +1,9 @@
 a=list(input().lower().replace(' ',''))
 b=list(input().lower().replace(' ',''))
 d=[]
-
 for i in a:
     if i in b and i not in d:
         d.append(i)
 d.sort()
 d=''.join(d)
-if(d):
-    print(d)
-else:
-    print('-1')
+print(d) if d else print('-1')
