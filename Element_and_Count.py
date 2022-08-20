1ec63cb4d9f7f989c4f87b3e2cef209e064c9c5b
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-08-20 16:28:49 +0530

    a=int(input()) s=list(map(int,input().split())) d=[] for i in s:     if i not in d:         d.append(i) for i in d:     print(i,s.count(i),end=' ')

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,11 +1,8 @@
 a=int(input())
 s=list(map(int,input().split()))
-c=[]
+d=[]
 for i in s:
-    if i not in c:
-        c.append(i)
-h=[]
-for i in c:
-    h.append(i)
-    h.append(s.count(i))
-print(*h)
+    if i not in d:
+        d.append(i)
+for i in d:
+    print(i,s.count(i),end=' ')
