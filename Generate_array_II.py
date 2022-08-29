b70deffa95114e5a3b4146a2d078567775c5aab6
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-08-29 10:58:14 +0530

    n=int(input()) s=list(map(int,input().split())) f,d=[],[] for i in range(n):     if(i%2==0):         f.append(s[i])     else:         d.append(s[i]) for i in range(len(f)):     for j in range(d[i]):         print(f[i],end=' ')

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -1,9 +1,11 @@
-a=int(input())
+n=int(input())
 s=list(map(int,input().split()))
-d,l=[],[]
-for i in range(0,len(s),2):
-    d.append(s[i])
-    l.append(s[i+1])
-for i in range(len(d)):
-    for j in range(l[i]):
-        print(d[i],end=' ')
+f,d=[],[]
+for i in range(n):
+    if(i%2==0):
+        f.append(s[i])
+    else:
+        d.append(s[i])
+for i in range(len(f)):
+    for j in range(d[i]):
+        print(f[i],end=' ')
