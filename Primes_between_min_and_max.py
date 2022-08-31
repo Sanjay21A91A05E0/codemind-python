60fa63998a7aba28cef17e3219d2dd37c04e328a
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-08-31 15:55:30 +0530

    def prime(a):     c=0     for i in range(1,a+1):         if(a%i==0):             c+=1     if(c==2):         return 1     else:         return 0 a=int(input()) s=list(map(int,input().split())) k=s.index(min(s)) f=s.index(max(s)) c=0 if(k>f):     t=k     k=f     f=t for i in range(k,f+1):     if prime(s[i]):         c+=1 print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -1,23 +1,22 @@
 def prime(a):
-    if(a==1):
-        return 0
-    elif(a==2):
-        return 1
-    for i in range(2,int(a**0.5)+1):
+    c=0
+    for i in range(1,a+1):
         if(a%i==0):
-            return 0
-    else:
+            c+=1
+    if(c==2):
         return 1
+    else:
+        return 0
 a=int(input())
 s=list(map(int,input().split()))
 k=s.index(min(s))
-d=s.index(max(s))
-if(k>d):
-    t=k
-    k=d
-    d=t
+f=s.index(max(s))
 c=0
-for i in range(k,d+1):
-    if(prime(s[i])):
+if(k>f):
+    t=k
+    k=f
+    f=t
+for i in range(k,f+1):
+    if prime(s[i]):
         c+=1
 print(c)
