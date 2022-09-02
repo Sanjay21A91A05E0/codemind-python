a4a86e24eb3d84eaf6492dc170df73fb42b53f4f
Sanjay kumar <97424311+sanjaykumar7481@users.noreply.github.com>
2022-09-02 13:43:30 +0530

    s=input().lower().split() g='aeiou' c=0 for i in s:     k=len(i)     for j in range(k//2):         if (i[j] in g and i[k-j-1] not in g) or(i[j]not in g and i[k-j-1] in g):             c+=1 print(c)

--- count_vowel_consonent_pairs_in_words.py
+++ count_vowel_consonent_pairs_in_words.py
@@ -1,10 +1,9 @@
-a=input().lower().split()
-s='aeiou'
+s=input().lower().split()
+g='aeiou'
 c=0
-for i in a:
-    for j in range(len(i)//2):
-        if(i[j] in s and i[-j-1] not in s):
-            c+=1
-        elif(i[j]not in s and i[-j-1] in s):
+for i in s:
+    k=len(i)
+    for j in range(k//2):
+        if (i[j] in g and i[k-j-1] not in g) or(i[j]not in g and i[k-j-1] in g):
             c+=1
 print(c)
